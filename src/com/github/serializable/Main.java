b80f7753d08d7092e132bce495201bc9fc9f8857
Magnus <EnderCrypt@gmail.com>
2015-10-23 16:02:30 +0200

    made start code that creates the file repositories

--- src/com/github/serializable/Main.java
+++ src/com/github/serializable/Main.java
@@ -1,14 +1,18 @@
 package com.github.serializable;
 
 import com.github.serializable.collection.file.FileOrderRepository;
+import com.github.serializable.collection.file.FileProductRepository;
+import com.github.serializable.collection.file.FileUserRepository;
 
 public class Main
 {
 
 	public static void main(String[] args)
 	{
-		// test example
-		FileOrderRepository fop = new FileOrderRepository("Repository/Order/");
+		// create file repositories
+		FileUserRepository userRep = new FileUserRepository("Repository/User/");
+		FileOrderRepository orderRep = new FileOrderRepository("Repository/Order/");
+		FileProductRepository productRep = new FileProductRepository("Repository/Product/");
 	}
 
 }
