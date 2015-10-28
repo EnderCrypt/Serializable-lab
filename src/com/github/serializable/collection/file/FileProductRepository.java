cbfc67714519cf63c47fcf32a03aa094947706e8
Magnus <EnderCrypt@gmail.com>
2015-10-28 16:06:35 +0100

    fixed fileRepo

--- src/com/github/serializable/collection/file/FileProductRepository.java
+++ src/com/github/serializable/collection/file/FileProductRepository.java
@@ -26,11 +26,7 @@ public class FileProductRepository implements ProductRepository
 	{
 		// init variables
 		saveDirectory = new File(directory);
-<<<<<<< HEAD
 		saveFile = new File(saveDirectory + "/data");
-=======
-		saveFile = new File(saveDirectory+"/data");
->>>>>>> 9df5a9fba9cb404e0bab8723727fd14e1f84e38f
 		// create directory
 		if (!saveDirectory.exists())
 		{
