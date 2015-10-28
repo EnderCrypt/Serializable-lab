05f2b1b072db25fb6f5c6a503ae52f21281f8ffc
Timmie <timmie_camal@hotmail.com>
2015-10-28 13:24:01 +0100

    created user/prod/order

--- src/com/github/serializable/collection/data/User.java
+++ src/com/github/serializable/collection/data/User.java
@@ -8,10 +8,55 @@ import java.io.Serializable;
 public class User implements Serializable
 {
 	private static final long serialVersionUID = -4836684853320153893L;
-	
-	public User()
+	private String username;
+	private String password;
+	private String eMail;
+
+	public User(String username, String password, String eMail)
 	{
-		// TODO Auto-generated constructor stub
+		this.username = username;
+		this.password = password;
+		this.eMail = eMail;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public String geteMail()
+	{
+		return eMail;
+	}
+
+	@Override
+	public boolean equals(Object other)
+	{
+		if (this == other)
+		{
+			return true;
+		}
+		if (other instanceof User)
+		{
+			User otherUser = (User) other;
+			return username.equals(otherUser.username) && eMail.equals(otherUser.eMail);
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		int result = 1;
+		result += username.hashCode() * 37;
+		result += eMail.hashCode() * 37;
+
+		return result;
 	}
 
 }
